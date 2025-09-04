09cc57db496129b2769a4b3b5c98ea4e68ebfe11
Nioduro <mickkcx@proton.me>
2025-09-04 17:04:05 +0200

    feat: add endpoint to retrieve permissions for a specific user

--- backend/src/main/kotlin/wtf/ndu/vibin/routes/PermissionRoutes.kt
+++ backend/src/main/kotlin/wtf/ndu/vibin/routes/PermissionRoutes.kt
@@ -17,6 +17,13 @@ fun Application.configurePermissionRoutes() = routing {
             call.respond(permissions.map { it.id })
         }
 
+        getP("/api/permissions/{userId}", PermissionType.MANAGE_PERMISSIONS) {
+            val userId = call.parameters["userId"]?.toLongOrNull() ?: return@getP call.missingParameter("userId")
+            val user = UserRepo.getById(userId) ?: return@getP call.notFound()
+            val permissions = PermissionRepo.getPermissions(user.id.value)
+            call.respond(permissions.map { it.id })
+        }
+
         putP("/api/permissions/{userId}", PermissionType.MANAGE_PERMISSIONS) {
             val userId = call.parameters["userId"]?.toLongOrNull() ?: return@putP call.missingParameter("userId")
             val permissionId = call.request.queryParameters["permissionId"] ?: return@putP call.missingParameter("permissionId")
