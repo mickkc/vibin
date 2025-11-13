0cd4618203295bb4fdb3ad866eb1b4e92f1da94f
Nioduro <mickkcx@proton.me>
2025-11-13 20:53:09 +0100

    fix: add LyricsMetadataSource to server settings definitions

--- backend/src/main/kotlin/wtf/ndu/vibin/settings/server/SettingDefs.kt
+++ backend/src/main/kotlin/wtf/ndu/vibin/settings/server/SettingDefs.kt
@@ -18,6 +18,7 @@ val serverSettings = listOf(
     ArtistMetadataSource,
     AlbumMetadataFetchType,
     AlbumMetadataSource,
+    LyricsMetadataSource,
 )
 
 object WelcomeTexts : ServerSetting<List<String>>(
